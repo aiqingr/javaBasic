066407fbeb0b2bb818291ddba8ddaf57134069e8
Tongyang Ni <ntyaiqingr@gmail.com>
2020-02-26 20:57:41 -0500

    basic input(can not gitignore .vscode folder)

---
+++ javaBasicKnowledge/CalcCircleArea.java
@@ -0,0 +1,13 @@
+package javaBasicKnowledge;
+
+import java.util.Scanner;
+
+public class CalcCircleArea {
+    public static void main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入半径： ");
+        final double radius = scanner.nextDouble();
+        final double area = 3.14 * radius * radius;
+        System.out.println("圆的面积是： " + area);
+    }
+}
