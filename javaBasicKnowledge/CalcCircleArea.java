2bce9a379fcae920faef41adf2d3c3e5690cdaed
Tongyang Ni <ntyaiqingr@gmail.com>
2020-03-02 20:48:50 -0500

    modify

--- javaBasicKnowledge/CalcCircleArea.java
+++ javaBasicKnowledge/CalcCircleArea.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class CalcCircleArea {
     public static void main(final String[] args) {
+        System.out.println("This is my breakpoint!");
         final Scanner scanner = new Scanner(System.in);
         System.out.println("请输入半径： ");
         final double radius = scanner.nextDouble();
